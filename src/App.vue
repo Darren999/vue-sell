6b1dff5f102eda9d981a0220cb826cf7f6684a99
Darren.fan <2511132466@qq.com>
2019-03-23 22:50:16 +0800

    border-1px not done

--- src/App.vue
+++ src/App.vue
@@ -38,11 +38,14 @@ export default {
 </script>
 
 <style lang="scss">
+  /*@import "@/common/style/mixin.scss";*/
 .tab{
   display: flex;
   width:100%;
   height: 40px;
   line-height: 40px;
+  border-bottom: 1px solid rgba(7,17,27,0.1);
+  /*border-1px(rgba(7,17,27,0.1))*/
   .tab-item{
     flex: 1;
     text-align: center;
