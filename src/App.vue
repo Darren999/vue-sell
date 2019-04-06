7a7e9027ccd9fa18bc083103475503b64a4bd37c
darrren.jian <darrren@darrren-Mac.local>
2019-04-06 18:38:01 +0800

    add better-scroll

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
         <router-link to="/ratings" exact>评论</router-link>
       </div>
       <div class="tab-item">
-      <router-link to="/seller" exact>商家</router-link>
+        <router-link to="/seller" exact>商家</router-link>
       </div>
     </div>
     <router-view></router-view>
@@ -17,41 +17,46 @@
 </template>
 
 <script>
-import header from '@/components/header/header'
-export default {    data(){
-    return{
-      seller:{}
+  import header from '@/components/header/header'
+
+  export default {
+    data() {
+      return {
+        seller: {}
+      }
+    },
+    created() {
+      this.$http.get("/api/seller").then(response => {
+        this.seller = response.body.data
+        console.log(this.seller)
+      }, response => {
+        console.log("get data eror")
+      })
+    },
+    components: {
+      'v-header': header
     }
-  },
-  created() {
-    this.$http.get("/api/seller").then(response =>{
-      this.seller = response.body.data
-      console.log(this.seller)
-    },response =>{
-      console.log("get data eror")
-    })
-  },
-  components:{
-    'v-header':header
   }
-}
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
   @import "./common/stylus/mixin.styl"
   .tab
     display: flex
-    width:100%
+    width: 100%
     height: 40px
     line-height: 40px
-    border-1px(rgba(7,17,127,1))
+    border-1px(rgba(7, 17, 127, 1))
+
     .tab-item
       flex: 1
       text-align: center
+
       & > a
         display: block
         font-size: 14px
-        color:rgb(77,85,93)
+        color: rgb(77, 85, 93)
+
         &.router-link-active
-          color: rgb(240,20,20)
+          color: rgb(240, 20, 20)
 </style>
--- src/router/router.js
+++ src/router/router.js
@@ -2,11 +2,11 @@ import goods from '@/components/goods/goods'
 import ratings from '@/components/ratings/ratings'
 import seller from '@/components/seller/seller'
 
-const Routes= [
+const Routes = [
   { path: '/', component: goods },
   { path: '/goods', component: goods },
-    { path: '/ratings', component: ratings },
-    { path: '/seller', component: seller }
+  { path: '/ratings', component: ratings },
+  { path: '/seller', component: seller }
 ]
 
 export default Routes
