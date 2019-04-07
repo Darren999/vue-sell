6ccc041aea1214ef8fd5e3a6923c77d920e2c7fe
darrren.jian <darrren@darrren-Mac.local>
2019-04-07 16:50:25 +0800

    add shopcart

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -28,7 +28,7 @@
                 </div>
                 <div class="price">
                   <span class="now">¥{{food.price}}</span>
-                  <span class="old" v-if="food.oldPrice">¥{{food.oldPrice}}</span>
+                  <span class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
                 </div>
               </div>
             </li>
@@ -36,13 +36,20 @@
         </li>
       </ul>
     </div>
+    <shopcart :deliveryPrice="seller.deliveryPrice" :minPrice="seller.minPrice"></shopcart>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   import BScroll from 'better-scroll'
+  import shopcart from '@/components/shopcart/shopcart'
 
   export default {
+    props: {
+      seller:{
+        type: Object,
+      }
+    },
     data() {
       return {
         goods: [],
@@ -101,17 +108,20 @@
       },
       _calculateHeight() {
         let foodlist = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook');
-        console.log("foodlist:"+foodlist+"length:"+foodlist.length);
+        // console.log("foodlist:"+foodlist+"length:"+foodlist.length);
         let height = 0;
         this.listHeight.push(height);
         for(let i=0; i < foodlist.length; i++){
           let item = foodlist[i];
           height += item.clientHeight;
-          console.log("i:"+i+" height:"+height);
+          // console.log("i:"+i+" height:"+height);
           this.listHeight.push(height);
         }
       },
     },
+    components: {
+      shopcart
+    }
   }
 </script>
 
@@ -125,12 +135,10 @@
     bottom: 46px
     width: 100%
     overflow: hidden
-
     .menu-wrapper
       flex: 0 0 80px
       width: 80px
       background: #f3f5f7
-
       .menu-item
         display: table
         height: 54px
@@ -153,32 +161,24 @@
           margin-right: 2px
           background-size: 12px 12px
           background-repeat: no-repeat
-
           &.decrease
             bg-image('decrease_3')
-
           &.discount
             bg-image('discount_3')
-
           &.guarantee
             bg-image('guarantee_3')
-
           &.invoice
             bg-image('invoice_3')
-
           &.special
             bg-image('special_3')
-
         .text
           display: table-cell
           width: 56px
           vertical-align: middle
           border-1px(rgba(7, 17, 27, 0.1))
           font-size: 12px
-
     .foods-wrapper
       flex: 1
-
       .title
         padding-left: 14px
         height: 26px
@@ -187,53 +187,42 @@
         font-size: 12px
         color: rgb(147, 153, 159)
         background: #f3f5f7
-
       .food-item
         display: flex
         margin: 18px
         padding-bottom: 18px
         border-1px(rgba(7, 17, 27, 0.1))
-
         &:last-child
           border-none()
           margin-bottom: 0
-
         .icon
           flex: 0 0 57px
           margin-right: 10px
-
         .content
           flex: 1
-
           .name
             margin: 2px 0 8px 0
             height: 14px
             line-height: 14px
             font-size: 14px
             color: rgb(7, 17, 27)
-
           .desc, .extra
             line-height: 10px
             font-size: 10px
             color: rgb(147, 153, 159)
-
           .desc
             line-height: 12px
             margin-bottom: 8px
-
           .extra
             .count
               margin-right: 12px
-
           .price
             font-weight: 700
             line-height: 24px
-
             .now
               margin-right: 8px
               font-size: 14px
               color: rgb(240, 20, 20)
-
             .old
               text-decoration: line-through
               font-size: 10px
